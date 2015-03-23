42a316060872cbfe1819b0d566b345e78ebf295d
LukasPJ <ljorgensen@winterleafentertainment.com>
2015-03-23 21:46:46 +0100

    Updated C-Interface

--- engine/source/c-interface/c-interface.cpp
+++ engine/source/c-interface/c-interface.cpp
@@ -7,20 +7,49 @@ CInterface& CInterface::GetCInterface()
    return INSTANCE;
 }
 
-char* CInterface::CallFunction(const char* name, const char **argv, int argc)
+bool CInterface::isMethod(const char* className, const char* methodName)
 {
-   return GetCInterface()._CallFunction(name, argv, argc);
+   return GetCInterface()._isMethod(className, methodName);
 }
 
-
-char* CInterface::_CallFunction(const char* name, const char **argv, int argc)
+char* CInterface::CallFunction(const char* name, const char **argv, int argc, bool *result)
 {
-   if (mCallback){
-      return mCallback(name, argv, argc);
+   return GetCInterface()._CallFunction(name, argv, argc, result);
+}
+
+char* CInterface::CallMethod(SimObject* obj, const char* name, const char **argv, int argc, bool *res)
+{
+   return GetCInterface()._CallMethod(obj->getClassName(), obj->getId(), name, argv, argc, res);
+}
+
+bool CInterface::_isMethod(const char* className, const char* methodName)
+{
+   if (mIsMethodCallback){
+      return mIsMethodCallback(className, methodName);
    }
    return NULL;
 }
 
-void SetCallFunctionCallback(void* ptr) {
+char* CInterface::_CallFunction(const char* name, const char **argv, int argc, bool *result)
+{
+   if (mFunctionCallback){
+      return mFunctionCallback(name, argv, argc, result);
+   }
+   result = false;
+   return NULL;
+}
+
+char* CInterface::_CallMethod(const char* className, U32 object, const char* name, const char **argv, int argc, bool *res)
+{
+   if (mMethodCallback){
+      return mMethodCallback(className, object, name, argv, argc, res);
+   }
+   res = false;
+   return NULL;
+}
+
+void SetCallbacks(void* ptr, void* methodPtr, void* isMethodPtr) {
    CInterface::GetCInterface().SetCallFunctionCallback(ptr);
+   CInterface::GetCInterface().SetCallMethodCallback(methodPtr);
+   CInterface::GetCInterface().SetCallIsMethodCallback(isMethodPtr);
 }
--- engine/source/c-interface/c-interface.h
+++ engine/source/c-interface/c-interface.h
@@ -2,26 +2,38 @@
 #ifndef C_INTERFACE_H
 #define C_INTERFACE_H
 
-#include "platform\platform.h"
+#include "platform/platform.h"
+#include "sim/simObject.h"
 
 extern "C" {
-   DLL_PUBLIC void SetCallFunctionCallback(void* ptr);
+   DLL_PUBLIC void SetCallbacks(void* ptr, void* methodPtr, void* isMethodPtr);
 }
 
 #define CALL_CINTERFACE_FUNCTION(name, ...){const char *v[] = { __VA_ARGS__ }; CInterface::CallFunction(name, v, sizeof(v) / sizeof(*v));}
 
 class CInterface {
-   typedef char* (*CallFunctionCallback)(const char* name, const char **argv, int argc);
-   CallFunctionCallback mCallback;
-   char* _CallFunction(const char* name, const char **argv, int argc);
+   typedef bool (*IsMethodCallback)(const char* className, const char* methodName);
+   typedef char* (*CallFunctionCallback)(const char* name, const char **argv, int argc, bool *result);
+   typedef char* (*CallMethodCallback)(const char* className, U32 object, const char* name, const char **argv, int argc, bool *result);
+   IsMethodCallback mIsMethodCallback;
+   CallFunctionCallback mFunctionCallback;
+   CallMethodCallback mMethodCallback;
+   char* _CallFunction(const char* name, const char **argv, int argc, bool *result);
+   char* _CallMethod(const char* className, U32 object, const char* name, const char **argv, int argc, bool *res);
+   bool _isMethod(const char* className, const char* methodName);
 public:
    CInterface(){
-      mCallback = NULL;
+      mFunctionCallback = NULL;
+      mMethodCallback = NULL;
    }
 
-   static char* CallFunction(const char* name, const char **argv, int argc);
+   static char* CallFunction(const char* name, const char **argv, int argc, bool *result);
+   static char* CallMethod(SimObject* obj, const char* name, const char **argv, int argc, bool *res);
+   static bool isMethod(const char* className, const char* methodName);
    static CInterface& GetCInterface();
-   void SetCallFunctionCallback(void* ptr) { mCallback = (CallFunctionCallback)ptr; };
+   void SetCallFunctionCallback(void* ptr) { mFunctionCallback = (CallFunctionCallback)ptr; };
+   void SetCallMethodCallback(void* ptr) { mMethodCallback = (CallMethodCallback)ptr; };
+   void SetCallIsMethodCallback(void* ptr) { mIsMethodCallback = (IsMethodCallback)ptr; };
 };
 
 #endif // C-INTERFACE_H
